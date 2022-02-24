184f5b0c4166c7dcf2ca482e4f023f3d824c97c4
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-02-24 11:10:17 -0300

    feat - Arquivo de estilos globais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return(
-    <h1>hello world</h1>
+    <>
+      <GlobalStyle/>
+      <h1>hello world!</h1>
+    </>
   )
 }
 
